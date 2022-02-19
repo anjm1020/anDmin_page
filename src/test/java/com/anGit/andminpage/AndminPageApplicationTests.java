f3c9f690fb2794d3d1e1a1fccc39b0bc1e7a36cc
GitJaeMin <55350092+anjm1020@users.noreply.github.com>
2022-02-19 16:03:48 +0900

    create : craete project

---
+++ src/test/java/com/anGit/andminpage/AndminPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anGit.andminpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndminPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
