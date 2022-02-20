bb20ce5c4e87835fa9301d58efe93d309d9ebecf
GitJaeMin <55350092+anjm1020@users.noreply.github.com>
2022-02-20 10:31:00 +0900

    Feat : itempage 템플릿 생성, itempage 컨트롤러 추가

--- src/main/java/com/anGit/andminpage/controller/ItemController.java
+++ src/main/java/com/anGit/andminpage/controller/ItemController.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,8 +27,16 @@ public class ItemController {
         return "item/itemlist";
     }
 
+    @GetMapping("/{itemId}")
+    public String itemInfo(@PathVariable long itemId, Model model) {
+        Optional<Item> item = itemRepository.findById(itemId);
+        model.addAttribute("item", item.get());
+        return "item/item";
+    }
+
     @PostConstruct
     public void init() {
+
         itemRepository.save(new Item("itemA", 1000, 10));
         itemRepository.save(new Item("itemB", 2000, 20));
         itemRepository.save(new Item("itemC", 3000, 30));
