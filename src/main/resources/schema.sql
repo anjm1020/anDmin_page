d896d60d043e33d1e6abea08fecfc27007ea93b4
GitJaeMin <55350092+anjm1020@users.noreply.github.com>
2022-03-02 15:02:31 +0900

    Fix : item table ddl 변경 - pk auto_increment 설정 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS ITEM;
 
 CREATE TABLE ITEM
 (
-    id integer NOT NULL,
+    id integer AUTO_INCREMENT NOT NULL,
     name varchar(255) NOT NULL,
     price integer,
     quantity integer,
